fae2b046d64a4292eee8d9cfeea112c8c8bca7e1
John <jpena91@gmail.com>
2019-06-13 18:20:56 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
 #
-# Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Pizzaplace.create(name: "Dumbo Pizza", address: "81 Prospect Place, Brooklyn, NY", long: 40.700885000, lat: -73.987448000, average_rating: 4)
+Pizzaplace.create(name: "Champion Pizza", address: "2 W 14th NY, NY", long: 73.994040000, lat: 40.736020000, average_rating: 3)
